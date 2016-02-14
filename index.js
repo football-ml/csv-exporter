9827d84d5940da501fc1ef35882ed0fb0d866f4c
Markus Ziller <markus.ziller@netlight.com>
2016-02-14 19:46:56 +0100

    Adds -M, --clubmeta [b] to index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ commander
   .option('-l, --league [s]', 'The league, i.e. 1 for Premier League, Serie A, 1. Bundesliga,.. [default=1]', '1')
   .option('-e, --exclude [s]', 'Excludes a certain attribute from the generated CSV, i.e. "form_delta_last_3;team_h_form_last_5" [default=[]]', list, [])
   .option('-m, --minmatches [n]', 'The minimum number of matches that have to be played before a match is considered for the training data [default=5]', toInt, 5)
+  .option('-M, --clubmeta [b]', 'NOTE: Currently only supported for (de,15,1). Crawls club meta data (marketValue..) and adds it to the data set. Decreases processing speed [default=false]', false)
   .option('-L, --local [b]', 'Use local data instead of github etc. [default=false]', false)
   .option('-C, --complete [b]', 'Also adds yet unplayed matches to the CSV. [default=false]', false)
   .option('-V, --verbose [b]', 'Also adds verbose data like team code that makes reading data easier for humans. [default=false]', false)
@@ -33,6 +34,7 @@ const behaviourConf = {
   verbose: commander.verbose,
   exclude: commander.exclude,
   minmatches: commander.minmatches,
+  clubmeta: commander.clubmeta,
   complete: commander.complete,
   tables: commander.tables
 };
@@ -51,10 +53,17 @@ const start = Date.now();
 co(function *() {
   const io = new IO(ioConf);
   yield io.loadClubAndMatchData(commander.local);
-  yield io.loadClubMeta();
 
-  const csvBuilder = new CSVBuilder(io.rounds, io.clubCodes, behaviourConf);
-  const data = csvBuilder.makeDataForCSVExport();
+  if (behaviourConf.clubmeta) {
+    io.clubMeta = yield io.loadClubMeta();
+    logger.info('Got club metadata from transfermarkt.de for %s clubs', Object.keys(io.clubMeta).length);
+  }
+
+  io.roundMeta = yield io.loadRoundMeta(1, 10);
+  logger.info('Got round metadata from transfermarkt.de for %s rounds', Object.keys(io.roundMeta).length);
+
+  const csvBuilder = new CSVBuilder(io, behaviourConf);
+  const data = yield csvBuilder.makeDataForCSVExport();
 
   io.writeToDiskAsCSV(data);
 
