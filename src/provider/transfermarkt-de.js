c1d4c3a183064b282b892a1f0f1151d5fbc24676
Markus Ziller <markus.ziller@netlight.com>
2016-02-14 19:45:49 +0100

    Adds getMatchdayInfo to transfermarkt-de.js

--- src/provider/transfermarkt-de.js
+++ src/provider/transfermarkt-de.js
@@ -7,7 +7,8 @@ const cheerio = require('cheerio');
 
 logger.cli();
 
-const idMapping = require('./transfermarkt-team-ids.json');
+const teamIdMapping = require('./transfermarkt-team-ids.json');
+const leagueIdMapping = require('./transfermarkt-league-ids.json');
 
 class TransfermarktProxy {
   static marketValueStringToNumber(marketValueString) {
@@ -18,13 +19,30 @@ class TransfermarktProxy {
 
     const split = marketValueString.split(' ');
     const factor = split[1] === 'Mio.' ? 1000000 : 1000;
-    const value = parseFloat(split[0].replace(',', '.'));
+    const value = lodash.toNumber(split[0].replace(',', '.'));
 
     return factor * value;
   }
 
+  static tmTeamIdToTeamCode(tmId) {
+    for (const id in teamIdMapping) {
+      if (teamIdMapping[id].id === tmId) {
+        return id;
+      }
+    }
+
+    return '';
+  }
+
   static stringToInt(string) {
     return lodash.toSafeInteger(lodash.trim(string));
+  }
+
+  static stringPercentageToDouble(string) {
+    const cleansedString = string.replace(' %', '').replace(',', '.');
+    const value = lodash.toNumber(cleansedString);
+
+    return value / 100;
   }
 
   static stringToDouble(string) {
@@ -36,10 +54,15 @@ class TransfermarktProxy {
     return lodash.toNumber(lodash.trim(properFormat));
   }
 
-  // http://www.transfermarkt.de/1-bundesliga/spieltag/wettbewerb/L1/plus/?saison_id=2015&spieltag=22 (Liga 1)
   // http://www.transfermarkt.de/fc-bayern-munchen/startseite/verein/27/saison_id/2015 <- Bayern (=27), 2015
   static getTeamInfo(teamCode, season) {
-    const teamId = idMapping[teamCode].id;
+    if (!teamIdMapping[teamCode]) {
+      return new Promise((resolve, reject) => {
+        resolve({});
+      });
+    }
+
+    const teamId = teamIdMapping[teamCode].id;
     const url = `http://www.transfermarkt.de/fc-bayern-munchen/startseite/verein/${teamId}/saison_id/${season}`;
     const info = {
       teamCode
@@ -68,8 +91,57 @@ class TransfermarktProxy {
         });
     });
   }
-}
 
-TransfermarktProxy.getTeamInfo('FCB', '2015');
+  // http://www.transfermarkt.de/1-bundesliga/spieltag/wettbewerb/L1/plus/?saison_id=2015&spieltag=22 (Liga 1)
+  static getMatchdayInfo(country, league, season, matchday) {
+
+    const leagueInTMTerminology = leagueIdMapping[country][league];
+
+    const tmNameShort = leagueInTMTerminology[0];
+    const tmNameLong = leagueInTMTerminology[1];
+    const url = `http://www.transfermarkt.de/${tmNameLong}/spieltag/wettbewerb/${tmNameShort}/plus`;
+
+    return new Promise((resolve, reject) => {
+      superagent
+        .get(url)
+        .query({
+          saison_id: season,
+          spieltag: matchday
+        })
+        .end((err, res) => {
+          if (err) {
+            return reject(err);
+          }
+
+          const $ = cheerio.load(res.text);
+          const playdayInfo = [];
+
+          $('.tm-user-tendenz').each((i, elem) => {
+            const tr = $(elem);
+            const tbody = tr.parent();
+
+            const homeTeamId = tbody.find('.hide-for-small').eq(0).find('.vereinprofil_tooltip').eq(0).attr('id');
+            const awayTeamId = tbody.find('.hide-for-small').eq(3).find('.vereinprofil_tooltip').eq(0).attr('id');
+
+            const winHomeRaw = tr.find('.bar-sieg').html();
+            const remisRaw = tr.find('.bar-remis').html();
+            const winAwayRaw = tr.find('.bar-niederlage').html();
+
+            const parsed = {
+              homeTeam: TransfermarktProxy.tmTeamIdToTeamCode(homeTeamId),
+              awayTeam: TransfermarktProxy.tmTeamIdToTeamCode(awayTeamId),
+              tmWinHomeUserProb: TransfermarktProxy.stringPercentageToDouble(winHomeRaw),
+              tmRemisUserProb: TransfermarktProxy.stringPercentageToDouble(remisRaw),
+              tmWinAwayUserProb: TransfermarktProxy.stringPercentageToDouble(winAwayRaw)
+            };
+
+            playdayInfo.push(parsed);
+          });
+
+          resolve(playdayInfo);
+        });
+    });
+  }
+}
 
 module.exports = TransfermarktProxy;
